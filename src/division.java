125152a28aad1ddbc9db3b88387bca2924eddb6e
HugoRey0806 <hugoreyzas777@gmail.com>
2025-10-28 12:40:10 +0100

    HOTFIX: Validación para evitar división entre 0

--- src/division.java
+++ src/division.java
@@ -1,12 +1,10 @@
 public class division {
-    public static double dividir(double a, double b) throws ArithmeticException {
-        System.out.println("Programador C: Ejecutando división mejorada");
-        if (b == 0) {
-            throw new ArithmeticException("Error: No se puede dividir entre cero");
-        }
-        return a / b;
+public static double dividir(double a, double b) {
+    if (b == 0) {
+        throw new ArithmeticException("Error: No se puede dividir entre 0");
     }
-    
+    return a / b;
+}
     public static double divisionSegura(double a, double b) {
         if (b == 0) {
             System.out.println("Advertencia: División entre cero. Retornando 0.");
