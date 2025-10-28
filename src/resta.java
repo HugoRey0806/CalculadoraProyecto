ca39a9b7842c43413b5bca93724b67508199bfcd
HugoRey0806 <hugoreyzas777@gmail.com>
2025-10-28 12:11:06 +0100

    A: Implementación de suma y resta

--- src/resta.java
+++ src/resta.java
@@ -1,5 +1,15 @@
 public class resta {
-    public double operar(double a, double b) {
+    public static double restar(double a, double b) {
+        System.out.println("Programador A: Ejecutando resta mejorada");
         return a - b;
+    }
+    
+    public static double restarMultiple(double... numeros) {
+        if (numeros.length == 0) return 0;
+        double resultado = numeros[0];
+        for (int i = 1; i < numeros.length; i++) {
+            resultado -= numeros[i];
+        }
+        return resultado;
     }
 }
--- src/suma.java
+++ src/suma.java
@@ -1,5 +1,14 @@
 public class suma {
-    public double operar(double a, double b) {
+    public static double sumar(double a, double b) {
+        System.out.println("Programador A: Ejecutando suma mejorada");
         return a + b;
+    }
+    
+    public static double sumarMultiple(double... numeros) {
+        double resultado = 0;
+        for (double num : numeros) {
+            resultado += num;
+        }
+        return resultado;
     }
 }
